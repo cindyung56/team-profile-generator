d136fb11b9cc5f2faec6e0d1d2393ca8ca2aee45
cindy <cindyung56@gmail.com>
2022-09-16 15:31:42 -0700

    added links to github and emails

--- src/card-template.js
+++ src/card-template.js
@@ -1,22 +1,4 @@
-function generateManagerCard(data){
-return `
-<div class="card col-lg-3 col-md-4 col-sm-6 p-1 m-2">
-<div class="bg-primary text-white p-3">
-    <h2 class="card-title ml-2">${data.getName()}</h2>
-    <h5 class="card-subtitle ml-2">${data.getRole()}</h5>
-</div>
-<div class="card-body bg-light">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${data.getId()}</li>
-        <li class="list-group-item">Email: ${data.getEmail()}</li>
-        <li class="list-group-item">Office Number: ${data.officeNumber}</li>
-      </ul>
-</div>
-</div>
-`
-} 
-
-function generateEngineerCard(data){
+function generateInitialInfo(data, lastSegment){
     return `
     <div class="card col-lg-3 col-md-4 col-sm-6 p-1 m-2">
     <div class="bg-primary text-white p-3">
@@ -26,31 +8,28 @@ function generateEngineerCard(data){
     <div class="card-body bg-light">
         <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${data.getId()}</li>
-            <li class="list-group-item">Email: ${data.getEmail()}</li>
-            <li class="list-group-item">GitHub Username: ${data.getGithub()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${data.getEmail()}">${data.getEmail()}</a></li>
+            ${lastSegment}
           </ul>
     </div>
     </div>
-    `
-    } 
+    `    
+}
+
+function generateManagerCard(data){
+    const lastSegment = `<li class="list-group-item">Office Number: ${data.officeNumber}</li>`
+    return generateInitialInfo(data, lastSegment);
+} 
+
+function generateEngineerCard(data){
+    const lastSegment = `<li class="list-group-item">GitHub Username: <a href="https://github.com/${data.getGithub()}">${data.getGithub()}</a></li>`
+    return generateInitialInfo(data, lastSegment);
+} 
 
 function generateInternCard(data){
-    return `
-    <div class="card col-lg-3 col-md-4 col-sm-6 p-1 m-2">
-    <div class="bg-primary text-white p-3">
-        <h2 class="card-title ml-2">${data.getName()}</h2>
-        <h5 class="card-subtitle ml-2">${data.getRole()}</h5>
-    </div>
-    <div class="card-body bg-light">
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${data.getId()}</li>
-            <li class="list-group-item">Email: ${data.getEmail()}</li>
-            <li class="list-group-item">School: ${data.getSchool()}</li>
-            </ul>
-    </div>
-    </div>
-    `
-    } 
+    const lastSegment = `<li class="list-group-item">School: ${data.getSchool()}</li>`
+    return generateInitialInfo(data, lastSegment);
+} 
 
 
 module.exports = {generateManagerCard, generateEngineerCard, generateInternCard};
